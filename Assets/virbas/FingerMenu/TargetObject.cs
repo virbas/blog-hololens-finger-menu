a284291cc5cbbc77a917388ea940b64ce659dc28
Linas Ozeraitis <virbas@gmail.com>
2022-11-22 13:06:13 +0100

    add preview object manipulation

---
+++ Assets/virbas/FingerMenu/TargetObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Apprentice.Components;
+
+public class TargetObject : MonoBehaviour
+{
+  public void ChangeColor(ThumbButton button)
+  {
+    var material = button.transform.Find("Geometry").GetComponent<Material>();
+    GetComponent<MeshRenderer>().material.color = material.color;
+  }
+  
+  public void ChangeGeometry(ThumbButton button)
+  {
+    var mesh = button.transform.Find("Geometry").GetComponent<MeshFilter>().mesh;
+    GetComponent<MeshFilter>().mesh = mesh;
+  }
+
+  public void ChangeGeometry(Mesh mesh)
+  {
+    GetComponent<MeshFilter>().mesh = mesh;
+  }
+}
